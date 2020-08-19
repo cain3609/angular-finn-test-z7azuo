e4f5d204f37b7629e2664fbbdf0ee2adf471d9fd
cain3609 <miss.fin@yahoo.com.tw>
2020-08-19 16:48:54 +0800

    1 file modified, 1 added

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+//import { CommonModule } from '@angular/common';
+import{RouterModule, Routes} from '@angular/router';
+import {HeroesComponent} from './heroes/heroes.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
